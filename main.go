fa103c6f2cb5bdfadeb45d41712cf07ef789b774
mytkom <marek.mytkowski.mm@gmail.com>
2023-12-14 12:07:52 +0100

    init

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+    router := gin.Default()
+
+    router.GET("/", func(c *gin.Context) {
+        fmt.Printf("%+v\n", c)
+        c.JSON(http.StatusOK, gin.H{
+            "message": "pong",
+        })
+    })
+
+    router.Run()
+}
