c4288d7893f03daabdb2e09488ec5cff7fedc0a2
rodzena <agata.rodzen@ynd.co>
2023-12-15 11:35:50 +0100

    challenge show again

--- challenge/challenge.go
+++ challenge/challenge.go
@@ -2,7 +2,6 @@ package challenge
 
 import (
 	"fmt"
-	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -22,39 +21,46 @@ const (
 )
 
 type Challenge struct {
-    common.Model
-    ChallengerID  int `gorm:"not null"json:"-"`
-    Challenger    user.User `json:"challenger"`
-	ChallengedID  int `gorm:"not null"json:"-"`
-    Challenged    user.User `json:"challenged"`
-    Title         string `gorm:"not null"json:"title"`
-    Amount        uint   `gorm:"not null;default:0"json:"amount"`
-    Status        Status `gorm:"not null;default:0"json:"status"`
-    ChallengerWon *bool `json:"challenger_won"`
+	common.Model
+	ChallengerID  int       `gorm:"not null"json:"-"`
+	Challenger    user.User `json:"challenger"`
+	ChallengedID  int       `gorm:"not null"json:"-"`
+	Challenged    user.User `json:"challenged"`
+	Title         string    `gorm:"not null"json:"title"`
+	Amount        uint      `gorm:"not null;default:0"json:"amount"`
+	Status        Status    `gorm:"not null;default:0"json:"status"`
+	ChallengerWon *bool     `json:"challenger_won"`
 }
 
 func Show(c *gin.Context, db *gorm.DB) {
-	userid := c.Param("userid")
-	message := "userid is " + userid
-	c.String(http.StatusOK, message)
-	fmt.Println(message)
+	var cha Challenge
+
+	id, _ := c.Params.Get("id")
+	rows_affected := db.Find(&cha, id).RowsAffected
+
+	if rows_affected == 0 {
+		c.JSON(422, "Challenge not found")
+		return
+	} else {
+		c.JSON(200, gin.H{"challenge": cha})
+	}
 }
 
 func Update(c *gin.Context, db *gorm.DB) {
-    cId, err := strconv.ParseUint(c.Param("id"), 10, 64)
-    if err != nil {
-        c.JSON(422, "Cannot parse given ID")
-        return
-    }
+	cId, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(422, "Cannot parse given ID")
+		return
+	}
 
-    chall := Challenge{}
-    ra := db.Preload("Challenger").Preload("Challenged").Find(&chall, cId).RowsAffected
-    if ra == 0 {
-        c.JSON(422, "Challenge not found")
-        return
-    }
+	chall := Challenge{}
+	ra := db.Preload("Challenger").Preload("Challenged").Find(&chall, cId).RowsAffected
+	if ra == 0 {
+		c.JSON(422, "Challenge not found")
+		return
+	}
 
-    c.JSON(200, chall)
+	c.JSON(200, chall)
 }
 
 func Index(c *gin.Context, db *gorm.DB) {
