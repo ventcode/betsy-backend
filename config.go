1fba93b57e2295cf50fa2218894c2b845d797902
Mateusz Lata <mateusz.lata@ynd.co>
2023-12-15 12:55:13 +0100

    app config

--- config.go
+++ config.go
@@ -3,22 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-const (
-	dbHost     = "localhost"
-	dbPort     = 5433
-	dbUser     = "postgres"
-	dbPassword = "password"
-	dbName     = "postgres"
-)
+type AppConfig struct {
+	DatabaseConfig
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Name     string
+}
 
 func DatabaseConnection() *gorm.DB {
-	format := "host=%s port=%d user=%s password=%s dbname=%s sslmode=disable"
-	dsn := fmt.Sprintf(format, dbHost, dbPort, dbUser, dbPassword, dbName)
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		appConfig.DatabaseConfig.Host,
+		appConfig.DatabaseConfig.Port,
+		appConfig.DatabaseConfig.User,
+		appConfig.DatabaseConfig.Password,
+		appConfig.DatabaseConfig.Name,
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -26,4 +39,29 @@ func DatabaseConnection() *gorm.DB {
 	}
 
 	return db
+}
+
+func ENV(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func SetAppConfig() AppConfig {
+	dbPort, _ := strconv.Atoi(ENV("DATABASE_PORT"))
+	appConfig := AppConfig{
+		DatabaseConfig: DatabaseConfig{
+			Host:     ENV("DATABASE_HOST"),
+			Port:     dbPort,
+			User:     ENV("DATABASE_USER"),
+			Password: ENV("DATABASE_PASSWORD"),
+			Name:     ENV("DATABASE_NAME"),
+		},
+	}
+
+	return appConfig
 }
