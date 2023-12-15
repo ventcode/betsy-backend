f9c533e21ed5706bbfef2fad58876f4ff110afad
mytkom <marek.mytkowski.mm@gmail.com>
2023-12-15 13:28:49 +0100

    All but logic

--- bet/bet.go
+++ bet/bet.go
@@ -1,7 +1,6 @@
 package bet
 
 import (
-	"github.com/ventcode/betsy-backend/challenge"
 	"github.com/ventcode/betsy-backend/user"
 	"github.com/ventcode/betsy-backend/common"
 )
@@ -11,7 +10,6 @@ type Bet struct {
     UserID          int `gorm:"not null"json:"-"`
     User            user.User `json:"user"`
     ChallengeID     int `gorm:"not null"json:"challenge_id"`
-    Challenge       challenge.Challenge `json:"-"`
     BetOnChallenger bool `gorm:"not null"json:"bet_on_challenger"`
     Amount          uint `gorm:"not null;check:amount > 0"json:"amount"`
 }
--- challenge/challenge.go
+++ challenge/challenge.go
@@ -3,9 +3,9 @@ package challenge
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ventcode/betsy-backend/bet"
 	"github.com/ventcode/betsy-backend/common"
 	"github.com/ventcode/betsy-backend/user"
 	"gorm.io/gorm"
@@ -23,13 +23,19 @@ const (
 
 type Challenge struct {
     common.Model
-    ChallengerID  int `gorm:"not null"json:"-"`
+    ChallengerID  int `gorm:"not null" json:"-"`
     Challenger    user.User `json:"challenger"`
-	ChallengedID  int `gorm:"not null"json:"-"`
+	ChallengedID  int `gorm:"not null" json:"-"`
     Challenged    user.User `json:"challenged"`
-    Title         string `gorm:"not null"json:"title"`
-    Amount        uint   `gorm:"not null;default:0"json:"amount"`
-    Status        Status `gorm:"not null;default:0"json:"status"`
+    Title         string `gorm:"not null" json:"title"`
+    Amount        uint   `gorm:"not null;default:0" json:"amount"`
+    Status        Status `gorm:"not null;default:0" json:"status"`
+    ChallengerWon *bool `json:"challenger_won"`
+    Bets          []bet.Bet `json:"bets"`
+}
+
+type UpdateChallengeInput struct {
+    Status        *Status `json:"status" binding:"required,max=3"`
     ChallengerWon *bool `json:"challenger_won"`
 }
 
@@ -41,17 +47,58 @@ func Show(c *gin.Context, db *gorm.DB) {
 }
 
 func Update(c *gin.Context, db *gorm.DB) {
-    cId, err := strconv.ParseUint(c.Param("id"), 10, 64)
-    if err != nil {
-        c.JSON(422, "Cannot parse given ID")
+    chall := Challenge{}
+    ra := db.Preload("Challenger").
+        Preload("Challenged").
+        Preload("Bets").
+        Find(&chall, c.Param("id")).RowsAffected
+    if ra == 0 {
+        c.JSON(404, "Challenge not found")
         return
     }
 
-    chall := Challenge{}
-    ra := db.Preload("Challenger").Preload("Challenged").Find(&chall, cId).RowsAffected
-    if ra == 0 {
-        c.JSON(422, "Challenge not found")
+    if chall.Status == Finished {
+        c.JSON(422, "Challenge is Finished, can't change anything!")
         return
+    }
+
+    updateInput := UpdateChallengeInput{}
+    err := c.ShouldBindJSON(&updateInput)
+    if err != nil {
+        c.JSON(422, err.Error())
+        return
+    }
+
+    if *updateInput.Status != Finished  {
+        if updateInput.ChallengerWon != nil {
+            c.JSON(422, "You can't determine who won if it is not finished!")
+            return
+        }
+    } else if updateInput.ChallengerWon == nil {
+        c.JSON(422, "You need to specify challenger_won!")
+        return
+    }
+
+    err = db.Model(&chall).Updates(updateInput).Error
+    if err != nil {
+        c.JSON(422, err)
+        return
+    }
+
+    if chall.Status == Finished {
+        db.Transaction(func(db *gorm.DB) error {
+            // TODO: calculate sum of bets
+
+            for i := range chall.Bets {
+                if chall.Bets[i].BetOnChallenger && *chall.ChallengerWon {
+                    // TODO: winner logic and check if .Bets[i].User is preloaded - I doubt it
+                    // chall.Bets[i].User.MoneyAmount += chall.Bets[i].Amount
+                }
+                db.Updates(&chall.Bets[i].User)
+            }
+
+            return nil
+        })
     }
 
     c.JSON(200, chall)
